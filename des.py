7fd70559924a71fc169d1d22335a5eb07b824e6d
rahul1410 <60419103+rahul1410@users.noreply.github.com>
2020-01-29 15:01:03 +0530

    Create des.py

---
+++ des.py
@@ -0,0 +1,36 @@
+print("Welcome to Uber Available destinstion are Ambattur,Guindy,Koyambedu")
+print("\n Ambattur:amb\n Guindy:gui\n Koyambedu:cmbt")
+d=raw_input("Enter your destination:")
+if('d==amb'):
+   print("Source:Avadi")
+   print("Destination:Ambattur")
+   print("Total Fare:30")
+   print("Total Km:12")
+elif(d=='gui'):
+   print("Source:Poonammallee")
+   print("Destination:Guindy")
+   print("Total Fare:20")
+   print("Total Km:15")
+elif(d=='cmbt'):
+   print("Source=Kumananchavadi")
+   print("Destination:Koyambedu")
+   print("Total Fare:30")
+   print("Total Km:30")
+   
+   
+   
+   ''''
+   output
+   Welcome to Uber Available destinstion are Ambattur,Guindy,Koyambedu
+
+ Ambattur:amb
+ Guindy:gui
+ Koyambedu:cmbt
+Enter your destination:Ambattur
+Source:Avadi
+Destination:Ambattur
+Total Fare:30
+Total Km:12
+
+
+'''
